9fe4b372882a70eb2a83ff2cbb66198c112c824e
ruser <ruser@192.168.11.2>
2021-04-06 23:43:47 +0900

    JpaMysql

---
+++ SpringBootJpaMysql/src/main/java/kr/sity/SpringBootJpaMysql/Product.java
@@ -0,0 +1,36 @@
+package kr.sity.SpringBootJpaMysql;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Product {
+
+	@Id
+    @GeneratedValue // Auto IncId Generator.
+    private Long id;
+
+    @Column(length = 20, nullable = false)
+    private String name;
+
+    @Column(columnDefinition = "TEXT", nullable = true)
+    private Integer price;
+
+    private String memo;
+
+    @Builder
+    public Product(String name, Integer price, String memo) {
+        this.name = name;
+        this.price = price;
+        this.memo = memo;
+    }
+}
---
+++ SpringBootJpaMysql/src/main/java/kr/sity/SpringBootJpaMysql/ProductController.java
@@ -0,0 +1,32 @@
+package kr.sity.SpringBootJpaMysql;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class ProductController {
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	@GetMapping("/create")  
+	public void create(Product product) {
+	productRepository.save(product);
+	}
+	
+	@GetMapping("/readOne")
+	public Optional readOne(Long id) {
+	return productRepository.findById(id);
+	}
+
+	@GetMapping("/readAll")
+	public List readAll() {
+	return productRepository.findAll();
+	}
+
+}
